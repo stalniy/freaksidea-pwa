69e38c20e86fbf3404e00fffaea1de48cbe6ef51
Sergii <sergiy.stotskiy@gmail.com>
2020-09-30 12:46:40 +0300

    chore(sw): restore service worker

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,12 +3,12 @@ import { register } from './serviceWorker';
 
 window.__isAppExecuted__ = true;
 const app = bootstrap('fi-app');
-// register({
-//   onUpdate(worker) {
-//     app.notify('updateAvailable', {
-//       onClick() {
-//         worker.postMessage({ type: 'SKIP_WAITING' });
-//       }
-//     });
-//   }
-// });
+register({
+  onUpdate(worker) {
+    app.notify('updateAvailable', {
+      onClick() {
+        worker.postMessage({ type: 'SKIP_WAITING' });
+      }
+    });
+  }
+});
