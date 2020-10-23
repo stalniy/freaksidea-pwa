1d8fb94d9db4d78fc093e67875c038c73d511d88
Sergii <sergiy.stotskiy@gmail.com>
2020-10-23 22:32:22 +0300

    fix(analytics): makes sure env var is used in all required places

--- tools/index.html.js
+++ tools/index.html.js
@@ -10,7 +10,7 @@ function includeGA(id) {
   }
 
   return `
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-19088556-6"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=${id}"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
